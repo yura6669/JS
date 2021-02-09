bc65f4edde19ede89b3204c7f493064eb4c6bf24
Yura S <yura.shiyan01022000@gmail.com>
2021-02-09 21:32:47 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+//alert("Hello Word");
+
+// var answer = confirm("Are you sure");
+// console.log(answer);
+
+
+// var answer = prompt("Чи є вам 18?", "Так");
+// console.log(typeof(answer));
